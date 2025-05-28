8f9a4572a2b4ebcb264ea90ffb6547219dad4ad7
Carlos O <orjuelacarlos80@gmail.com>
2025-05-27 19:29:32 -0500

    avanecs

--- src/api/companies/controller/companies.js
+++ src/api/companies/controller/companies.js
@@ -4,18 +4,24 @@ import hubspot from "@hubspot/api-client";
 export class Companies {
   static migrate(req, res) {
     // send status to HubSpot
-    res.sendStatus(204);
+    res.sendStatus(204)
 
     const data = req.body[0];
 
     // verify if body isn't empty
     if (data == null) return res.status(400).json({ message: "missing data" });
 
-    console.log("tipo de request", data);
+    console.log("Request type", data);
 
     // Determine the subscription type to execute the corresponding function
     if (data.subscriptionType == "company.creation") {
         Companies.create(data.objectId);
+    }
+
+    if (data.subscriptionType == "company.propertyChange") {
+        const {objectId, propertyName, propertyValue} = data
+        Companies.update(objectId, propertyName, propertyValue);
+        
     }
   }
 
@@ -47,11 +53,72 @@ export class Companies {
         
         const apiResponse2 = await targetHubspotClient.crm.companies.basicApi.create(SimplePublicObjectInputForCreate);
 
-        console.log(JSON.stringify(apiResponse2, null, 2));
-          
+        console.log("Company successfully migrated")
+        
+        return
         
     } catch (error) {
         console.error("COMPANY CREATE ERROR", error)
     }
+  }
+
+  static async update(objId, property, value){
+    console.log("update company values ", objId, property, value)
+        try {
+            console.log("Updating company")
+
+            //find location ID of company
+            const originHubspotClient = new hubspot.Client({"accessToken":KEYS.ORIGIN});
+
+            //company properties
+            const allProperties = ["location_id"];
+
+
+            const apiResponse = await originHubspotClient.crm.companies.basicApi.getById(objId, allProperties);
+
+
+            //delete certain data
+            const {location_id} = apiResponse.properties
+
+            
+            //Find target company
+
+            const hubspotClient = new hubspot.Client({"accessToken":KEYS.TARGET});
+
+
+            const PublicObjectSearchRequest = {
+                filterGroups: [
+                    {
+                        filters:[
+                            {
+                                "propertyName": "location_id",
+                                "operator": "CONTAINS_TOKEN",
+                                "value": location_id
+                            }
+                        ]
+                    }
+                ]    
+            };
+
+            const foundCompany = await hubspotClient.crm.companies.searchApi.doSearch(PublicObjectSearchRequest);
+            
+            
+            //update company data
+
+            const properties = {}
+            properties[property] = value
+            const SimplePublicObjectInput = { properties };
+            const companyId = foundCompany.results[0].id;
+
+            
+            const finalResponse = await hubspotClient.crm.companies.basicApi.update(companyId, SimplePublicObjectInput);
+
+            console.log("company successfully updated")
+            
+            return
+            
+        } catch (error) {
+            console.error("COMPANY UPDATE ERROR", error)
+        }
   }
 }
